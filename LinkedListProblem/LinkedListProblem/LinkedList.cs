4dbc1ddaff0e7a2bf8616b50db0846cb25eb36cb
mohan <sahumohan338@gmail.com>
2022-08-12 00:55:28 -0700

    [mohan]add: create linked list

---
+++ LinkedListProblem/LinkedListProblem/LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine("inserted data: " + node.data);
+            }
+
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListProblem
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(56);
+            list.Add(30);
+            list.Display();
+        }
+    }
+}
